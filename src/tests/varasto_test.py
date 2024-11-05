2b90249d9c680da1631d5db0181ddb9a4c539a84
mlenni <lenni.martikainen@gmail.com>
2024-11-05 14:49:56 +0200

    add poetry

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -37,4 +37,55 @@ class TestVarasto(unittest.TestCase):
         self.varasto.ota_varastosta(2)
 
         # varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
-        self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 4)
+        
+    def test_lisaa_varastoon_yli(self):
+        self.varasto.lisaa_varastoon(12)  
+        self.assertAlmostEqual(self.varasto.saldo, 10)  
+
+    def test_ota_varastosta_yli(self):
+        self.varasto.lisaa_varastoon(5)
+        otettu = self.varasto.ota_varastosta(7) 
+        self.assertAlmostEqual(otettu, 5)  
+        self.assertAlmostEqual(self.varasto.saldo, 0)  
+
+    def test_lisaa_negatiivinen(self):
+        self.varasto.lisaa_varastoon(-5)  
+        self.assertAlmostEqual(self.varasto.saldo, 0)  
+
+    def test_ota_negatiivinen(self):
+        self.varasto.ota_varastosta(-3)  
+        self.assertAlmostEqual(self.varasto.saldo, 0) 
+    
+    def test_nolla_kapasiteetti(self):
+        varasto_zero = Varasto(0)
+        self.assertAlmostEqual(varasto_zero.tilavuus, 0)
+        self.assertAlmostEqual(varasto_zero.saldo, 0)
+    
+    def test_poista_nolla(self):
+        initial_saldo = self.varasto.saldo
+        self.varasto.ota_varastosta(0)
+        self.assertAlmostEqual(self.varasto.saldo, initial_saldo)
+
+    def test_lisaa_tayteen(self):
+        self.varasto.lisaa_varastoon(10)
+        self.varasto.lisaa_varastoon(1) 
+        self.assertAlmostEqual(self.varasto.saldo, 10)
+
+    def test_ota_tyhjasta(self):
+        self.varasto.ota_varastosta(1)
+        self.assertAlmostEqual(self.varasto.saldo, 0)  
+        
+    def test_init_ali(self):
+        varasto_negative_saldo = Varasto(10, -5)
+        self.assertAlmostEqual(varasto_negative_saldo.saldo, 0)  
+
+    def test_init_yli(self):
+        varasto_exceeding_saldo = Varasto(10, 15)
+        self.assertAlmostEqual(varasto_exceeding_saldo.saldo, 10) 
+
+    def test_teksti(self):
+        self.varasto.lisaa_varastoon(5)
+        expected_str = "saldo = 5, vielä tilaa 5"
+        self.assertEqual(str(self.varasto), expected_str)        
+        
+  
